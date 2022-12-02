881905b850bb68f2baa9c9fbe9f8fae55ae65a51
Brandon Yu <brndonyu@Brandons-MBP.hsd1.ca.comcast.net>
2022-12-01 23:16:46 -0800

    created follow table (lists followees and followers)

--- myProject/app/models.py
+++ myProject/app/models.py
@@ -12,6 +12,7 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String, unique=True)
     password = db.Column(db.String(200))
 
+    #followstatus = db.Table()
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
 class Post(db.Model):
@@ -19,3 +20,11 @@ class Post(db.Model):
     post = db.Column(db.String(1000))
     link = db.Column(db.String(100))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id')) 
+
+class Follows(db.Model):
+    #2 wide table that lists followers (left) of followee (right)
+    # e.g. if x and y both follow each other,
+    # Table:    x | y
+    #           y | x
+    follower = db.Column(db.String, db.ForeignKey('user.id'), primary_key = True)
+    followee = db.Column(db.String, db.ForeignKey('user.id'), primary_key = True)
