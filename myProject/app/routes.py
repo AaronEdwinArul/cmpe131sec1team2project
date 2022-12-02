8499b6838ea2ccdb67ed461989ed330b7d5839fb
MichaelXiao9625 <michaelkrakow9625@gmail.com>
2022-12-01 18:27:53 -0800

    created login page along with basic home page

--- myProject/app/routes.py
+++ myProject/app/routes.py
@@ -1,23 +1,25 @@
 from app import myapp_obj
-from flask import render_template, redirect, flash
+from flask import render_template, redirect, flash, url_for, request
 from app.forms import LoginForm
+
+database={'michael':'123','brandon':'123','aaron':'123','vincent':'123'}
 
 @myapp_obj.route('/login', methods=['POST', 'GET'])
 def login():
     current_form = LoginForm()
-    # taking input from the user and doing somithing with it
-    if current_form.validate_on_submit():
-        flash('quick way to debug')
-        flash('another quick way to debug')
-        print(current_form.username.data, current_form.password.data)
-        return redirect('/')
+    username=request.form['username']
+    password=request.form['password']
+    print(request.form)
+    request.form.viewkeys()
     if current_form.username.data == "" or current_form.password.data == "":
-        flash('ERROR: Empty input')
-    a = 1
-    name = 'Carlos'
-    return render_template('login.html', name=name, a=a, form=current_form)
-
+        return render_template('login.html',info='Empty Input')
+    if username not in database:
+        return render_template('login.html',info='Invalid User')
+    if database[username]==password:
+        return render_template('home.html', name=username, form=current_form)
+    else:
+        return render_template('login.html',info='Invalid Password')
 
 @myapp_obj.route('/')
 def home():
-    return render_template('base.html')
+    return render_template('login.html')
