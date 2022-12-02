c6e1841b2a83aded6a1e5615f0c8eca6953959e3
Brandon Yu <brndonyu@Brandons-MBP.hsd1.ca.comcast.net>
2022-12-02 00:14:19 -0800

    added table for likes; finished database relationships

--- myProject/app/models.py
+++ myProject/app/models.py
@@ -12,19 +12,29 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String, unique=True)
     password = db.Column(db.String(200))
 
-    #followstatus = db.Table()
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    likes = db.relationship('Likes', backref = 'liker',lazy = 'dynamic')
+    follows = db.relationship('Follows', backref = 'follow', lazy = 'dynamic')
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    post = db.Column(db.String(1000))
+    post = db.Column(db.String(500))
     link = db.Column(db.String(100))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id')) 
 
+    source = db.relationship('Likes', backref = 'original', lazy = 'dynamic')
+
+class Likes(db.Model):
+    #2 wide table that lists users who liked (left) a post (right)
+    #posts are labelled by id
+    liked = db.Column(db.String, db.ForeignKey('user.id'),primary_key = True)
+    post = db.Column(db.String, db.ForeignKey('post.id'), primary_key = True)
+
 class Follows(db.Model):
     #2 wide table that lists followers (left) of followee (right)
-    # e.g. if x and y both follow each other,
+    # e.g. if x and y both follow each other, and z follows x
     # Table:    x | y
     #           y | x
+    #           z | x
     follower = db.Column(db.String, db.ForeignKey('user.id'), primary_key = True)
     followee = db.Column(db.String, db.ForeignKey('user.id'), primary_key = True)
