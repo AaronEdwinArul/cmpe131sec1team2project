e8b9cfbee6b6a05d492011708a0c8b68366c7546
Aaron Arul <aaron.arul@sjsu.edu>
2022-12-02 23:22:02 -0800

    aaron home page start

---
+++ myProject/app/forms.py
@@ -0,0 +1,18 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember me')
+    submit = SubmitField('Sign In')
+
+class HomePageForm(FlaskForm):
+    submitHome = SubmitField('Home')
+    
+
+
+class LogoutForm(FlaskForm):
+    submitLogout = SubmitField('Logout')
+
