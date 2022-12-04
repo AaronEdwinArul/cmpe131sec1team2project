4357035c39616d76edbe9004900d2af24548d197
MichaelXiao9625 <michaelkrakow9625@gmail.com>
2022-12-03 21:15:14 -0800

    added blob of progress

--- myProject/app/routes.py
+++ myProject/app/routes.py
@@ -1,25 +1,91 @@
 from app import myapp_obj
-from flask import render_template, redirect, flash, url_for, request
-from app.forms import LoginForm
-
-database={'michael':'123','brandon':'123','aaron':'123','vincent':'123'}
+from flask import render_template, redirect, flash, request
+from app.forms import LoginForm, SignupForm, PostForm, HomePageForm
+from app.models import User, Post, Likes, Follows, db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import current_user, login_user, logout_user, login_required
+from app import db
 
 @myapp_obj.route('/login', methods=['POST', 'GET'])
 def login():
     current_form = LoginForm()
-    username=request.form['username']
-    password=request.form['password']
-    print(request.form)
-    request.form.viewkeys()
-    if current_form.username.data == "" or current_form.password.data == "":
-        return render_template('login.html',info='Empty Input')
-    if username not in database:
-        return render_template('login.html',info='Invalid User')
-    if database[username]==password:
-        return render_template('home.html', name=username, form=current_form)
-    else:
-        return render_template('login.html',info='Invalid Password')
+    errorMessage = ''
+    if current_form.validate_on_submit():
+        user = User.query.filter_by(username=current_form.username.data).first()
+        if user is None or not db.user.check_password(current_form.password.data):
+            errorMessage = 'Invalid username or password'
+            return render_template('login.html', form=current_form, errorMessage=errorMessage)
+        login_user(user, remember=current_form.remember_me.data)
+        return redirect('/home')
+    return render_template('login.html', form=current_form, errorMessage=errorMessage)
+
+@myapp_obj.route('/logout')
+def logout():
+    logout_user()
+    return render_template('login.html')
+
+@myapp_obj.route('/home', methods=['POST', 'GET'])
+def home():
+    a = 8
+    name = 'Friend'
+    current_form = HomePageForm()
+    if current_form.validate_on_submit():
+       return redirect('/home')
+    return render_template('home.html', form=current_form)
+
+@myapp_obj.route('/signup', methods = ['POST','GET'])
+def create():
+    current_form = SignupForm()
+    errorMessage = ''
+    if current_form.validate_on_submit():   
+        if not(validPassword(current_form.password.data)):
+            errorMessage = 'Password must be longer than 8 characters'
+        elif not(validEmail(current_form.email.data)):
+            errorMessage = 'Invalid email address (must have domain .com,.org,.edu)'
+        else:
+            user = User()
+            user.first = generate_password_hash(current_form.first.data)
+            user.last = generate_password_hash(current_form.last.data)
+            user.email = generate_password_hash(current_form.email.data)
+            user.username = current_form.username.data
+            user.password = generate_password_hash(current_form.password.data)
+            db.session.add(user)
+            db.session.commit()
+            return redirect('/login')        #redirect to home page when implemented
+    return render_template('signup.html',form=current_form, error = errorMessage)
+
+@myapp_obj.route('/post', methods = ['POST','GET'])
+#@login_required
+def post():
+    current_form = PostForm()
+    if current_form.validate_on_submit():
+        post = Post()
+        post.post = current_form.text.data
+        post.link = current_form.link.data
+        post.user_id = 1
+        with myapp_obj.app_context():
+            db.session.add(post)
+            db.session.commit()
+        return redirect('/home')        #redirects to home after posting, will show post
+    return render_template('post.html', form = current_form)
 
 @myapp_obj.route('/')
-def home():
-    return render_template('login.html')
+def base():
+    return render_template('base.html')
+
+# helper functions
+
+def validPassword(string):
+    if len(string) < 8:
+        return False
+    return True
+
+def validEmail(string):
+    boolAddress = False
+    boolDomain = False
+    for i in string:
+        if i == '@':
+            boolAddress = True
+    if (string[len(string)-4:len(string)] == '.com') or (string[len(string)-4:len(string)]) == '.org' or (string[len(string)-4:len(string)] == '.edu'):
+        boolDomain = True
+    return boolAddress and boolDomain
