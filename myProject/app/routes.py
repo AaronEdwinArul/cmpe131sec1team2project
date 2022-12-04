00ac46c9e53ff20b78cfa1858a6e2a3df6ae5e12
Brandon Yu <brndonyu@Brandons-MacBook-Pro.local>
2022-12-04 12:06:48 -0800

    feed now displays authors

--- myProject/app/routes.py
+++ myProject/app/routes.py
@@ -1,13 +1,10 @@
 from app import myapp_obj, db
 from flask import render_template, redirect, flash
-from app.models import User, Post#, Likes, Follows
+from app.models import User, Post, load_user#, Likes, Follows
 from app.forms import LoginForm, HomePageForm, LogoutForm, PostsForm, SignupForm, PostForm
 from datetime import date
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import current_user
-from flask_login import login_required
-from flask_login import login_user
-from flask_login import logout_user
+from flask_login import current_user,login_required,login_user,logout_user
 
 @myapp_obj.route('/')
 def base():
@@ -25,39 +22,37 @@ def test1():
 @login_required
 def private():
     return 'Hi this is a private page'
-'''
+
 @myapp_obj.route('/logout')
 @login_required
 def logout():
     load_user(current_user)
     return redirect('/login')
-'''
+
 
 @myapp_obj.route('/login', methods=['POST', 'GET'])
 def login():
     current_form = LoginForm()
     errorMessage = ''
-    if current_form.validate_on_submit():
-        users = User.query.all()
-        for user in users:
-            if user.username == current_form.username.data:
-                if check_password_hash(user.password,current_form.password.data):
-                    login_user(user, remember=current_form.remember_me.data)
-                    return redirect('/home')
-            else:
-                errorMessage = 'Invalid Username or Password'
+    
+    if current_form.validate_on_submit():   #on submission,
+        user = User.query.filter_by(username=current_form.username.data).first()    
+        if not user == None:        #check if user exists in database
+            if check_password_hash(user.password,current_form.password.data):   #check is password is correct
+                login_user(user, current_form.remember_me.data) #if true, login
+                return redirect('/home')
+        else:
+            errorMessage = 'Invalid Username of Password'       #else, raise error message 
     return render_template('login.html', form=current_form, errorMessage=errorMessage)
-
+'''
 @myapp_obj.route('/logout')
 def logout():
     logout_user()
     return render_template('base.html')
-
+'''
 @myapp_obj.route('/home', methods=['POST', 'GET'])
 @login_required
 def home():
-    a = 8
-    name = 'Friend'
     current_form = HomePageForm()
     if current_form.validate_on_submit():
        return redirect('/home')
@@ -67,37 +62,32 @@ def home():
 def create():
     current_form = SignupForm()
     errorMessage = ''
-    if current_form.validate_on_submit():   
-        if not(validPassword(current_form.password.data)):
+    if current_form.validate_on_submit():           #on form submission,
+        if not(validPassword(current_form.password.data)):  #check if password is long enough
             errorMessage = 'Password must be longer than 8 characters'
-        elif not(validEmail(current_form.email.data)):
+        elif not(validEmail(current_form.email.data)):      #simple email check
             errorMessage = 'Invalid email address (must have domain .com,.org,.edu)'
         else:
-            user = User()
+            user = User()       #create db object with user's first & last name, email, username, password
             user.first = generate_password_hash(current_form.first.data)
             user.last = generate_password_hash(current_form.last.data)
             user.email = current_form.email.data
             user.username = current_form.username.data
             user.password = generate_password_hash(current_form.password.data)
-            db.session.add(user)
+            db.session.add(user)        #add user to database
             db.session.commit()
             return redirect('/login')        #redirect to home page when implemented
 
     return render_template('signup.html',form=current_form, error = errorMessage)
 
 @myapp_obj.route('/post', methods = ['POST','GET'])
-#@login_required
+@login_required
 def post():
-    '''
-    to do:
-    change user_id line when login status is implemented
-    '''
     current_form = PostForm()
     if current_form.validate_on_submit():
-        post = Post()
         post.post = current_form.text.data  #save body text to db
         post.link = current_form.link.data  #save image url to db
-        post.user_id = 1    #change to current user later on
+        post.user_id = current_user.id      #save current user's id to track poster
         today = date.today()
         post.date = str(today).replace('-','')      #date of post stored as yyyymmdd
         with myapp_obj.app_context():       #add object to db
@@ -107,11 +97,11 @@ def post():
     return render_template('post.html', form = current_form)
 
 @myapp_obj.route('/feed', methods = ['POST','GET'])
+@login_required
 def view():
 
     '''
     to do:
-    print username of author when login is implemented and current user can be found
     connect viewable posts to following list when implemented
     '''
 
@@ -119,9 +109,12 @@ def view():
     post = Post.query.all()     #query all posts
     posts = []                  #list of dictionaries
     for i in post:              #iterate through all queries
-        text = {}               #create a dictionary of 'body':'text'
+        text = {}               #create a dictionary of 'body':'text', etc.
         text['body'] = i.post
         text['link'] = i.link
+        text['id'] = i.user_id
+        text['author'] = i.get_author(i.user_id)
+        text['date'] = i.date
         posts.append(text)      #add individual dictionaries to array
     #/feed page will display each body text and have access to the link to show the image
     return render_template('feed.html', posts = posts)
